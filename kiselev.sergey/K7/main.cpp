9f711df0d2c155413b32e61daf83d8c8ecc158e3
deonik3 <kis.sergey2006@gmail.com>
2025-03-24 11:58:26 +0300

    work

--- kiselev.sergey/K7/main.cpp
+++ kiselev.sergey/K7/main.cpp
@@ -1,2 +1,57 @@
+#include <functional>
+template< class T, class Cmp = std::less< T > >
+struct BiTree
+{
+  T data;
+  Cmp cmp;
+  BiTree< T >* left;
+  BiTree< T >* right;
+  BiTree< T >* parent;
+};
+
+template< class T >
+struct BiTreeIterator
+{
+  BiTree< T >* node;
+
+  bool hasPrev() const;
+  bool hasNext() const;
+  BiTreeIterator< T > next() const;
+  BiTreeIterator< T > prev() const;
+
+  const T& data() const;
+};
+
+template< typename T >
+bool BiTreeIterator< T >::hasPrev() const
+{
+  BiTree< T >* temp = node;
+  if (temp->left)
+  {
+    return true;
+  }
+}
+
+template< typename T >
+bool BiTreeIterator< T >::hasNext() const
+{
+  BiTree< T >* temp = node;
+  if (temp->right)
+  {
+    return true;
+  }
+}
+
+template< typename T >
+void deleteTree(BiTree< T >* root)
+{
+  if (!root)
+  {
+    return;
+  }
+  deleteTree(root->left);
+  deleteTree(root->right);
+  delete root;
+}
 int main()
 {}
