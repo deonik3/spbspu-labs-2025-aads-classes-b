61b2b0f29a18bcf0154c0181adad1b0a3e8b6c19
deonik3 <kis.sergey2006@gmail.com>
2025-03-24 12:04:37 +0300

    push + find

--- kiselev.sergey/K7/main.cpp
+++ kiselev.sergey/K7/main.cpp
@@ -25,6 +25,10 @@ struct BiTreeIterator
 template< typename T >
 bool BiTreeIterator< T >::hasPrev() const
 {
+  if (!node)
+  {
+    return false;
+  }
   BiTree< T >* temp = node;
   if (temp->left)
   {
@@ -35,13 +39,72 @@ bool BiTreeIterator< T >::hasPrev() const
 template< typename T >
 bool BiTreeIterator< T >::hasNext() const
 {
+  if (!node)
+  {
+    return false;
+  }
   BiTree< T >* temp = node;
   if (temp->right)
   {
     return true;
   }
 }
+template< class T >
+std::pair< const BiTree< T >*, BiTree< T >* > findWithParent(const BiTree< T >* root, const T& value);
 
+template< typename T >
+const BiTree< T >* find(const BiTree< T >* root, const T& value);
+
+template< class T >
+BiTree< T >* pushTree(BiTree< T >* root, const T& value);
+
+template< typename T>
+const BiTree< T >* find(const BiTree< T >* root, const T& value)
+{
+  return findWithParent(root, value).first;
+}
+template< class T >
+std::pair< const BiTree< T >*, BiTree< T >* > findWithParent(const BiTree< T >* root, const T& value)
+{
+  BiTree< T >* parent = nullptr;
+  while (root)
+  {
+    parent = const_cast< BiTree< T >* >(root);
+    if (cmp(root->data, value))
+    {
+      root = root->right;
+    }
+    else if (cmp(value, root->data))
+    {
+      root = root->left;
+    }
+    else
+    {
+      return { root, parent };
+    }
+  }
+  return { nullptr, parent };
+}
+
+template< class T, class Cmp >
+BiTree< T >* pushTree(BiTree< T >* root, const T& value, Cmp cmp)
+{
+  if (!root)
+  {
+    root = new BiTree< T >{value, nullptr, nullptr };
+  }
+  BiTree< T >* temp = root;
+  BiTree< T >* parentTemp = findWithParent(temp, value, cmp).second;
+  if (cmp(value, parentTemp->data))
+  {
+    parentTemp->left = new BiTree< T >{ value, nullptr, nullptr };
+  }
+  else
+  {
+    parentTemp->right = new BiTree< T >{ value, nullptr, nullptr };
+  }
+  return root;
+}
 template< typename T >
 void deleteTree(BiTree< T >* root)
 {
