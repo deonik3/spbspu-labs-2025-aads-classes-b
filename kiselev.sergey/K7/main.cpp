795ece21153de82b9b5ad924b3ed0caa710b5b1f
deonik3 <kis.sergey2006@gmail.com>
2025-03-24 16:07:43 +0300

    Methods for iterators + output

--- kiselev.sergey/K7/main.cpp
+++ kiselev.sergey/K7/main.cpp
@@ -1,4 +1,6 @@
 #include <functional>
+#include <ostream>
+#include <stdexcept>
 template< class T, class Cmp = std::less< T > >
 struct BiTree
 {
@@ -34,6 +36,11 @@ bool BiTreeIterator< T >::hasPrev() const
   {
     return true;
   }
+  while (temp->parent && temp == temp->parent->left)
+  {
+    temp = temp->parent;
+  }
+  return !temp->parent;
 }
 
 template< typename T >
@@ -48,7 +55,68 @@ bool BiTreeIterator< T >::hasNext() const
   {
     return true;
   }
+  while (temp->parent && temp->parent->right)
+  {
+    temp = temp->parent;
+  }
+  return !temp->parent;
 }
+
+template< typename T >
+BiTreeIterator< T > BiTreeIterator< T >::next() const
+{
+  if (!hasNext())
+  {
+    throw std::logic_error("No next");
+  }
+  BiTree< T >* temp = node;
+  if (temp->right)
+  {
+    temp = temp->right;
+    while (temp->left)
+    {
+      temp = temp->left;
+    }
+    return BiTreeIterator< T >{ temp };
+  }
+  while (temp->parent && temp == temp->parent->right)
+  {
+    temp = temp->parent;
+  }
+  return BiTreeIterator< T >{ temp->parent };
+}
+
+template< typename T >
+BiTreeIterator< T > BiTreeIterator< T >::prev() const
+{
+  if (!hasPrev())
+  {
+    throw std::logic_error("No prev");
+  }
+  BiTree< T >* temp = node;
+  if (temp->left)
+  {
+    temp = temp->left;
+    while (temp->right)
+    {
+      temp = temp->right;
+    }
+    return BiTreeIterator< T >{ temp };
+  }
+  while (temp->parent && temp == temp->parent->left)
+  {
+    temp = temp->parent;
+  }
+  return BiTreeIterator< T >{ temp };
+}
+
+template< typename T >
+const T& BiTreeIterator< T >::data() const
+{
+  assert(!node);
+  return node->data;
+}
+
 template< class T >
 std::pair< const BiTree< T >*, BiTree< T >* > findWithParent(const BiTree< T >* root, const T& value);
 
@@ -115,6 +183,43 @@ void deleteTree(BiTree< T >* root)
   deleteTree(root->left);
   deleteTree(root->right);
   delete root;
+}
+
+template< typename T >
+std::ostream& output(std::ostream& out, BiTree< T >* root, const std::string& text)
+{
+  BiTreeIterator< T > it{ root };
+  if (text == "tomax")
+  {
+    while (it.hasPrev())
+    {
+      it = it.prev();
+    }
+    out << it.data();
+    it = it.next();
+    while (it.hasNext())
+    {
+      out << " " << it.data();
+      it = it.next();
+    }
+  }
+  else if (text == "tomin")
+  {
+    while (it.hasNext())
+    {
+      it = it.next();
+    }
+    out << it.data();
+    while (it.hasPrev())
+    {
+      out << " " << it.data();
+      it = it.prev();
+    }
+  }
+  else
+  {
+    throw std::logic_error("Unknown command");
+  }
 }
 int main()
 {}
